4ae54994ec2f84713300da09a97f6e22bd95e86e
Mustafa Zaki Assagaf <mustafa.segf@gmail.com>
2022-07-03 23:14:28 +0700

    feat: added delete topic on repo

--- pkg/chat/repository.go
+++ pkg/chat/repository.go
@@ -1,8 +1,9 @@
 package chat
 
 type Repo interface {
-	SendMessage(topic string, key string, message Message) (err error)
-  CheckTopic(topic string) (ok bool, err error)
-  CreateTopic(topic string) (err error)
-  Subscribe(topic string) (consumers chan Message, errors chan error, err error)
+	CheckTopic(topic string) (ok bool, err error)
+	CreateTopic(topic string) (err error)
+	DeleteTopic(topic string) (err error)
+	SendMessage(topic, key, text string) (err error)
+	Subscribe(topic string) (consumers chan Message, errors chan error, err error)
 }
